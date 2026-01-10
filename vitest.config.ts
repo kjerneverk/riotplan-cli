3a673e2d59a663add6b5b35f27f2e1db56fa06a8
Tim O'Brien <tobrien@discursive.com>
2026-01-10 12:39:53 -0800

    Separating Tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,20 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    include: ["src/**/*.test.ts", "tests/**/*.test.ts"],
+    include: ["tests/**/*.test.ts"],
     globals: true,
     coverage: {
       provider: "v8",
-      reporter: ["text", "html"],
+      reporter: ["text", "json", "html"],
       include: ["src/**/*.ts"],
+      exclude: [
+        "src/**/*.test.ts",
+        "node_modules/",
+        "dist/**",
+        "vitest.config.ts",
+        "vite.config.ts",
+        "eslint.config.mjs",
+      ],
     },
   },
 });
-
